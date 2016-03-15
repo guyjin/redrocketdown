4b7a4299a820319132d53b4f4d02d622a83d87c4
Ben Vaughan <benjamin.vaughan@gmail.com>
2016-03-15 00:06:15 -0600

    starting point.
    Base server.  Two empty pages.  It’s a start.

---
+++ index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var ejs = require('ejs');
+var logger = require('debug');
+
+var routes = require('./routes/index');
+var marsbase = require('./routes/marsbase');
+
+
+var rrd = express();
+
+
+// view engine setup
+
+rrd.set('view engine', 'ejs');
+
+
+
+rrd.use(express.static('public'));
+
+rrd.use('/', routes);
+rrd.use('/marsbase', marsbase);
+
+rrd.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404
+    next(err);
+});
+
+// // dev error handler will show stacktrace
+// if(rrd.get('env') === 'development') {
+//     rrd.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
+//
+// // prod error handler
+// // no stacktrace
+// rrd.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
+
+module.exports = rrd;
+
+rrd.listen(8080, function() {
+    console.log("RedRocketDown running on port 8080");
+})
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+/* GET Home Page */
+router.get('/', function(req,res,next) {
+    res.render('index');
+});
+
+module.exports = router;
