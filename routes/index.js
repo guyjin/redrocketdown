3e971e6ec8ae6629db4e016992daea314c07f9b1
Ben Vaughan <benjamin.vaughan@gmail.com>
2016-04-02 00:00:55 -0600

    template setup
    getting base styles for layout and normalize css included.

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ var path = require('path');
 var router = express.Router();
 
 var title = "Red Rocket Down";
+var pageClass = "home";
 
 /* GET Home Page */
 router.get('/', function(req,res,next) {
     res.render('index', {
-        "title":title
+        "title":title,
+        "pageClass":pageClass
     });
 });
 
